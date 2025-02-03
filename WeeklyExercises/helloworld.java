624e36c6ab079d4ddbaed2d507f6f1b2119b4430
c3704118 <F.Hasson-Wells3479@student.leedsbeckett.ac.uk>
2025-02-03 13:33:56 +0000

    main to man change

--- WeeklyExercises/helloworld.java
+++ WeeklyExercises/helloworld.java
@@ -5,7 +5,7 @@ public class helloworld
     //----------------------------------------------------
     //  Prints a statement.
     //----------------------------------------------------h
-    public static void main (String[] args)
+    public static void man (String[] args)
     {
         System.out.println ("An emergency broadcast");
     }
