2d2c6f24d5ee6d6509c735c04c99b0ee96a2f58d
c3704118 <F.Hasson-Wells3479@student.leedsbeckett.ac.uk>
2025-02-03 13:28:10 +0000

    wk1

---
+++ WeeklyExercises/helloworld.java
@@ -0,0 +1,12 @@
+package WeeklyExercises;
+
+public class helloworld
+{
+    //----------------------------------------------------
+    //  Prints a statement.
+    //----------------------------------------------------h
+    public static void main (String[] args)
+    {
+        System.out.println ("An emergency Broadcast");
+    }
+}
